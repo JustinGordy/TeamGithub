ebd37818ae24366311786f1467786945b22e4ede
TYLER LOVE <119451811+tylerlove1208@users.noreply.github.com>
2022-12-05 20:15:16 +0000

    image

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+    <?php echo '<p>Edited by Tyler</p>'; ?> 
 <img src="https://preview.redd.it/l74ex2asp6v61.jpg?auto=webp&s=911136fa734b3c83ac53e57cf1160aa98ff7574f" class="img-rounded" alt="Cinque Terre">
 
     <!--
