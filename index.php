176fcba0a4633b250589aa4f3b285e7b11f0e517
TYLER LOVE <119451811+tylerlove1208@users.noreply.github.com>
2022-12-06 19:33:26 +0000

    removed script

--- index.php
+++ index.php
@@ -6,12 +6,5 @@
     <h1>index.php</h1>
     <p>Edited by Tyler</p>
 <img src="https://preview.redd.it/l74ex2asp6v61.jpg?auto=webp&s=911136fa734b3c83ac53e57cf1160aa98ff7574f" class="img-rounded" alt="Cinque Terre">
-
-    <!--
-    This script places a badge on your repl's full-browser view back to your repl's cover
-    page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-    teal, blue, blurple, magenta, pink!
-    -->
-    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
   </body>
 </html>
