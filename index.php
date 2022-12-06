c40c2fdb4d3ee28fdd5d49de64461b77f89b13f0
TYLER LOVE <119451811+tylerlove1208@users.noreply.github.com>
2022-12-06 20:09:07 +0000

    added clickable image

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <body>
     <h1>index.php</h1>
     <p>Edited by Tyler</p>
-<img src="https://preview.redd.it/l74ex2asp6v61.jpg?auto=webp&s=911136fa734b3c83ac53e57cf1160aa98ff7574f" class="img-rounded" alt="Cinque Terre">
+    <a href="https://glogangworldwide.com/" target="_blank">
+    <img src="https://preview.redd.it/l74ex2asp6v61.jpg?auto=webp&s=911136fa734b3c83ac53e57cf1160aa98ff7574f" class="img-rounded" alt="Cinque Terre"> 
   </body>
 </html>
