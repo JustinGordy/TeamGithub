11a21118ff4d2e654aa14f395b2669ce4589f28e
TYLER LOVE <119451811+tylerlove1208@users.noreply.github.com>
2022-12-12 20:31:55 +0000

    made the form push data into a json file

--- redirect.php
+++ redirect.php
@@ -1,4 +1,40 @@
 <?php
-header('location: /form.php');
-echo json_encode();
+$jsonData = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/data.json");
+$json_encode = json_decode($jsonData , TRUE); // Converts the data into a PHP Array
+$jsonData = array();
+  
+  if(isset($_POST["BTN_submit"])) {
+
+$formData = array(
+  $fname = $_POST["fname"],
+  $lname = $_POST["lname"],
+  $email = $_POST["email"],
+  $number = $_POST["number"],
+  $know = $_POST["know"]
+);
+    $data=array(
+      'fname'=> $fname,
+      'lname'=> $lname,
+      'email'=> $email,
+      'number'=> $number,
+      'know'=> $know
+      
+    );
+
+    $json_encode[]=$data;
+      
+    $json_encode = json_encode($json_encode, JSON_PRETTY_PRINT);
+
+    file_put_contents('data.json', $json_encode);
+
+
+header('Location: /form.php');
+
+
+  
+  
+  }else{
+    //do nothing
+  }
+
 ?>
